2a9b25b4f80bc58a7084dbfaff61d694536ad000
Fifty Walrus <fiftywalrus@gmail.com>
2019-01-24 20:19:45 +0100

    Batch file fixes

--- Build SmoothOperator v1.0.bat
+++ Build SmoothOperator v1.0.bat
@@ -1,4 +1,8 @@
 for /f "delims== tokens=1,2" %%G in (filelocations.txt) do set %%G=%%H
 for /f "delims== tokens=1,2" %%G in (versionnames.txt) do set %%G=%%H
-"C:\Program Files (x86)\7-Zip\7z.exe" a -y -tzip "%texturepacks%\%v1.0%.zip" "*" -mx5
+del "%texturepacks%\%v1.0%.zip.bak"
+ren "%texturepacks%\%v1.0%.zip" "%v1.0%.zip.bak"
+del "%texturepacks%\%v1.0%.zip"
+cd 1.0
+"C:\Program Files (x86)\7-Zip\7z.exe" a -y -tzip "..\%texturepacks%\%v1.0%.zip" "*" -mx5
 pause
