adfede6f34ad94c2318caea67037a8638377b708
Fifty Walrus <fiftywalrus@gmail.com>
2019-01-24 20:32:51 +0100

    Version 1.11 added

---
+++ Build SmoothOperator v1.11.bat
@@ -0,0 +1,6 @@
+for /f "delims== tokens=1,2" %%G in (filelocations.txt) do set %%G=%%H
+for /f "delims== tokens=1,2" %%G in (versionnames.txt) do set %%G=%%H
+del "%resourcepacks%\%v1.11%.zip.bak"
+ren "%resourcepacks%\%v1.11%.zip" "%v1.11%.zip.bak"
+cd 1.11
+"C:\Program Files (x86)\7-Zip\7z.exe" a -y -tzip "..\%resourcepacks%\%v1.11%.zip" "*" -mx5
