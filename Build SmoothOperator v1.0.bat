f33e60afa6e9e09adb10bd9d35ee5377bbbf49f5
Fifty Walrus <fiftywalrus@gmail.com>
2019-01-31 18:19:09 +0100

    Added Bedrock Version 1.8
    Change to emtire folder structure and build bat files

--- Build SmoothOperator v1.0.bat
+++ Build SmoothOperator v1.0.bat
@@ -3,6 +3,6 @@ for /f "delims== tokens=1,2" %%G in (versionnames.txt) do set %%G=%%H
 del "%texturepacks%\%v1.0%.zip.bak"
 ren "%texturepacks%\%v1.0%.zip" "%v1.0%.zip.bak"
 del "%texturepacks%\%v1.0%.zip"
-cd 1.0
+cd "Java 1.0"
 "C:\Program Files (x86)\7-Zip\7z.exe" a -y -tzip "..\%texturepacks%\%v1.0%.zip" "*" -mx5
 pause
