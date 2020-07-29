afcab6190b0707315048ff6114d4542e6cce6737
Kalvin <kalvinlyle@gmail.com>
2020-07-29 17:58:25 +0200

    Delete Build Bedrock 1.8.bat

--- Build Bedrock 1.8.bat
+++
@@ -1,7 +0,0 @@
-for /f "delims== tokens=1,2" %%G in (filelocations.txt) do set %%G=%%H
-for /f "delims== tokens=1,2" %%G in (versionnames.txt) do set %%G=%%H
-del "%texturepacks%\%v1.8%.zip.bak"
-ren "%texturepacks%\%v1.8%.zip" "%v1.8%.zip.bak"
-cd "Bedrock 1.8"
-"C:\Program Files (x86)\7-Zip\7z.exe" a -y -tzip "..\%texturepacks%\%v1.8%.zip" "*" -mx5
-pause
